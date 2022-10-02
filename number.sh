f90e00ee2c99c725559163d55be9c88e969a7f79
Voffkait <111452610+Voffkait@users.noreply.github.com>
2022-10-02 23:48:16 +0300

    Update number.sh

--- number.sh
+++ number.sh
@@ -2,6 +2,6 @@ while True:
     def sum(a):
         return ((a - 1) / 2)
     print(f'чтобы выйти нажмите Ctrl и C')
-    a = int(input('Число: '))
+    a = int(input('Введите итоговое число: '))
 
     print(f'{sum(a)}')
